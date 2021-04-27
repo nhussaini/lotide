012463ac7612763b76363098714cea9a45775486
nhussaini <nasrullah_hussaini@yahoo.com>
2021-04-27 02:52:46 +0000

    completed assertEqauls function

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+
+/*
+Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
+Assertion Passed: 1 === 1
+*/
