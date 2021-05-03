ed3ab5d2c1a5971160de05fc814b97657f73a249
nhussaini <nasrullah_hussaini@yahoo.com>
2021-05-03 23:57:16 +0000

    finalized lotide

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
+//const countLetters = require('./countLetters');
+//const countOnly = require('./countOnly');
 
 module.exports = {
   head,
   tail,
   middle,
-  countLetters,
-  countOnly,
+  //countLetters,
+  //countOnly,
 
 };
